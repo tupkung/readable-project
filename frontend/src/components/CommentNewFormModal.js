3d9d67f01f32f8052e77fab7cda2382cedc0f433
Pheerawit Wasinphongwanit <pheerawit@gmail.com>
2017-09-27 00:08:10 +0700

    feat: add new commmentNewFormModal

--- frontend/src/components/CommentNewFormModal.js
+++ frontend/src/components/CommentNewFormModal.js
@@ -6,7 +6,11 @@ class CommentNewFormModal extends Component {
     render() {
         return (
             <div>
-
+                <div className={"modal " + (openModal ? "is-active" : "")}>
+                    <div className="modal-background"></div>
+                    <div className="modal-card"></div>
+                    <div className="modal-footer"></div>
+                </div>
             </div>
         );
     }
